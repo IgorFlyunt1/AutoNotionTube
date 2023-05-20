dc490adcbc6c711d1e66e30f27930fd62793d407
Igor Flyunt (EXT) <igor.flyunt@schindler.com>
2023-05-20 13:19:18 +0300

    added initial models

---
+++ src/AutoNotionTube.Domain/Entities/Video.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoNotionTube.Domain.Entities;
+
+public class Video
+{
+    public string FilePath { get; set; }
+    public string FileName { get; set; }
+    public bool UploadStatus { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateModified { get; set; }
+}
---
+++ src/AutoNotionTube.Domain/Entities/YouTubeVideo.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoNotionTube.Domain.Entities;
+
+public class YouTubeVideo
+{
+    public string VideoId { get; set; }
+    public string Url { get; set; }
+    public string Iframe { get; set; }
+    public string Transcript { get; set; }
+}
